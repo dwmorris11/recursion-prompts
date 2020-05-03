12200d46d5a7df9b8194879af516188667a95453
Dustin Morris <dustin.morris@outlook.com>
2020-05-03 17:15:22 -0500

    Completed through range on Apple

--- src/recursion.js
+++ src/recursion.js
@@ -77,8 +77,19 @@ var sumBelow = function(n) {
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
-};
 
+if (y-x === 0){
+  return [];
+}
+
+  var numbers = range(x+1, y);
+  if(x+1!==y){
+    numbers.unshift(x+1);
+  }
+  return numbers;
+
+};
+console.log(range(2,9));
 // 7. Compute the exponent of a number.
 // The exponent of a number says how many times the base number is used as a factor.
 // 8^2 = 8 x 8 = 64. Here, 8 is the base and 2 is the exponent.
