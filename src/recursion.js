78356c72c21a6afc4d00e96cebe0dc1140a5d286
Dustin Morris <dustin.morris@outlook.com>
2020-05-04 09:26:34 -0500

    Completed through multiply

--- src/recursion.js
+++ src/recursion.js
@@ -215,6 +215,39 @@ var modulo = function(x, y) {
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
 var multiply = function(x, y) {
+
+  if(y===0){return 0;}
+  if(x===0){return 0;}
+
+  if(x<0 && y<0){
+    x = -x;
+    y = -y;
+    return subply();
+  }
+
+  if(x>0 && y>0){
+    return subply();
+  }
+
+  if((x<0 || y<0) && !(x<0 && y<0)){
+    if(x<0){
+      return subply();
+    }else{
+      let tempX = x;
+      x = y;
+      y = tempX;
+      return subply();
+    }
+  }
+
+
+  function subply(){
+    if(y-1!==0){
+      return x + multiply(x,y-1);
+    }else{
+      return x;
+    }
+  }
 };
 
 // 13. Write a function that divides two numbers without using the / operator or
